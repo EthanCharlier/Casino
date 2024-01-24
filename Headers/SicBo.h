33b4882d4a4bd46a0e3657d5e7f794e94fe22dee
Ethan CHARLIER <ethan.charlier@gmail.com>
2024-01-24 09:55:37 +0100

    Add comment SicBo.h and SlotMachine.h

--- Headers/SicBo.h
+++ Headers/SicBo.h
@@ -1,30 +1,49 @@
-//
 // Created by ethan on 10/01/2024.
-//
 
+// Include necessary library for input/output operations.
 #include <iostream>
 #include <utility>
 #include <list>
 #include <map>
 
+// Header guard to prevent multiple inclusions of the same header file.
 #ifndef CASINO_SICBO_H
 #define CASINO_SICBO_H
 
+// Declaration of the SicBo class.
 class SicBo {
 public:
+    // Constructor that takes an initial balance as a parameter.
     SicBo(int initialBalance);
+
+    // Initialization function.
     bool init();
+
+    // Function to start the Craps game.
     void play();
+
+    // Function to retrieve the current balance.
     int getBalance();
 
 private:
+    // Private data members for the Craps class.
     int balance;
+
+    // Function to get user bets and amount to bet.
     std::pair<std::string, int> getBet();
+
+    // Function to simulate the rolling of three six-sided dice.
     std::list<int> rollDices();
+
+    // Function to display the result of the dice roll.
     std::map<int, int> displayRoll(std::list<int> diceList);
 
+    // Function to calculate the result of the Sic Bo game based on user bets and the dice roll.
     int calcResult(std::pair<std::string, int> bet, std::map<int, int> diceList);
+
+    // Function to calculate the sum of the three dice.
     int makeSum(std::map<int, int> diceList);
 };
 
+// End of the header guard.
 #endif //CASINO_SICBO_H
--- Headers/SlotMachine.h
+++ Headers/SlotMachine.h
@@ -1,23 +1,33 @@
-//
 // Created by ethan on 22/12/2023.
-//
 
+// Include necessary library for input/output operations.
 #include <string>
 #include <map>
 
+// Header guard to prevent multiple inclusions of the same header file.
 #ifndef CASINO_SLOTMACHINE_H
 #define CASINO_SLOTMACHINE_H
 
+// Declaration of the SlotMachine class.
 class SlotMachine {
 public:
+    // Constructor that takes an initial balance as a parameter.
     SlotMachine(int initialBalance);
+
+    // Initialization function.
     bool init();
+
+    // Function to start the Craps game.
     void play();
+
+    // Function to retrieve the current balance.
     int getBalance();
 
 private:
+    // Private data members for the Craps class.
     int balance;
 
+    // Arrays to store different types of symbols for the slot machine.
     const std::string symbolsTypeFruits[8] = {"🍇", "🍉", "🍊", "🍋", "🍌", "🍎", "🍒", "🥝"};
     const std::string symbolsTypeSweets[8] = {"🍦", "🍧", "🍨", "🍩", "🍪", "🍫", "🍬", "🍭"};
     const std::string symbolsTypeHearts[8] = {"🩷", "🧡", "💛", "💚", "💙", "🩵", "💜", "🤎"};
@@ -25,12 +35,21 @@ private:
     const std::string symbolsTypeWeather[8] = {"☁️", "⛅", "☀️", "🌨️", "🌩️", "🌪️", "🌧️", "🌤️"};
     const std::string symbolsTypeAnimals[8] = {"🐻", "🐨", "🐼", "🐵", "🐷", "🦊", "🐮", "🐯"};
 
+    // Function to prompt the user to choose a slot machine type.
     int slotMachineTypeChoice();
 
+    // Function to get the user's bet.
     int getBet();
+
+    // Function to simulate spinning the reels of the slot machine.
     void spinReels(std::string reels[5], int userChoice);
+
+    // Function to display the result of the slot machine reels.
     std::map<std::string, int> displayReels(std::string reels[5]);
+
+    // Function to calculate the result of the Slot Machine game based on the displayed symbols and user's bet.
     int calcResult(std::map<std::string, int> results, int bet);
 };
 
+// End of the header guard.
 #endif //CASINO_SLOTMACHINE_H
